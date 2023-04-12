033ae11c79a6d91e9a5a9829aebc9fa85c960f4b
utpalsinghdev <utpalsingh915@gmail.com>
2023-04-12 09:23:35 +0530

    prisma schema created and updted at added

--- src/Notes/notes.routes.ts
+++ src/Notes/notes.routes.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 import { addNotes } from "./notes.controller";
-import multer from "multer";
 
 const router = Router();
 
-const upload = multer({ dest: "uploads/" });
-router.post("/add/:id", upload.single("formData"), addNotes);
+router.post("/add/:id", addNotes);
 
 export default router;
